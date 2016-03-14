41c9c3035db963020423952289b496c934f668d8
zero <623368886@qq.com>
2016-03-14 22:06:35 +0800

    third commit

--- pixivImage.py
+++ pixivImage.py
@@ -1,105 +1,159 @@
+# -*- coding: utf-8 -*-
 __author__ = 'allsunny'
 # !python3
-# -*- coding: utf-8 -*-
+
 from spider import SpiderHTML
-import urllib, os, re
+import urllib
+import os
+import re
+import http
+import threading
+import queue
 from urllib import request
+from urllib import parse
+from urllib import error
 
 '''
 使用了第三方类库 BeautifulSoup4
 需要目录下的spider.py文件
 运行环境：python3.4,windows10
 功能：下载P站固定标签下的收藏超过一定数量的图片
-    标签(keyword)和收藏数目(bookmarkCount)可自行修改
+    标签(keyword)和收藏数目(bookmark_count)可自行修改
 '''
 
 # 使用前要修改cookie
-rootURL = 'http://www.pixiv.net'
-startURL = 'http://www.pixiv.net/search.php?order=date_d'
+ROOT_URL = 'http://www.pixiv.net'
+START_URL = 'http://www.pixiv.net/search.php?order=date_d&s_mode=s_tag_full'
 
-store_path = 'E:\\P站'
-cookie = '使用前要修改cookie'
-userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
+STORE_PATH = 'E:\\P站'
+COOKIE = '# 使用前要修改cookie'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
 
-class pixivImage(SpiderHTML):
-    def __init__(self, cookie, userAgent, keyword, dirName, bookmarkCount, startPage=1):
-        SpiderHTML.__init__(self, cookie, userAgent)
-        self.rootURL = rootURL
-        self.startURL = startURL
-        self.page = startPage
+
+class PixivImage(SpiderHTML, threading.Thread):
+    def __init__(self, keyword, dir_name, bookmark_count, start_page=1, end_page=1000):
+        SpiderHTML.__init__(self, COOKIE, USER_AGENT)
+        threading.Thread.__init__(self)
+        self.root_url = ROOT_URL
+        self.start_url = START_URL
+        self.start_page = start_page
+        self.end_page = end_page
         self.keyword = urllib.parse.quote(keyword)  # 将搜索关键词汉字转换为%xx
-        self.dirName = dirName
-        self.bookmarkCount = bookmarkCount
-        self.startURL += (u'&word=' + self.keyword + u'&p=')
+        self.dir_name = dir_name
+        self.bookmark_count = bookmark_count
+        self.start_url += (u'&word=' + self.keyword + u'&p=')
 
-    def start(self):
-        while(True):
-            pageURL = self.getPage()
-            imageItemList = self.getImageList(pageURL)
-            self.getImage(imageItemList)
+    def run(self):
+        for page in range(self.start_page, self.end_page + 1):
+            page_url = self.get_page_url(page)
+            image_item_list = self.get_image_list(page_url)
+            self.get_image(image_item_list)
 
-    def getPage(self):
-        pageURL = self.startURL + str(self.page)
-        print('Opening ', pageURL)
-        self.page += 1
-        return pageURL
+    def get_page_url(self, page):
+        page_url = self.start_url + str(page)
+        print('Opening ', page_url)
+        return page_url
 
-    def getImageList(self, pageURL):
-        content = self.getUrl(pageURL)
-        imageItemList = content.find_all(class_='image-item')
-        return imageItemList
+    def get_image_list(self, page_url):
+        content = self.get_url(page_url)
+        image_item_list = content.find_all(class_='image-item')
+        return image_item_list
 
-    def getImage(self, imageItemList):
-        for imageItem in imageItemList:
-            image = imageItem.find(class_='bookmark-count _ui-tooltip')
+    def get_image(self, image_item_list):
+        for image_item in image_item_list:
+            image = image_item.find(class_='bookmark-count _ui-tooltip')
             if image is None:
                 continue
 
-            bookmarkCount = int(image.get_text())    #收藏数目
-            if(bookmarkCount < self.bookmarkCount):
+            img_bookmark_count = int(image.get_text())  # 收藏数目
+            if img_bookmark_count < self.bookmark_count:
                 continue
             else:
-                imageId = imageItem.a['href']
-                self.ID = imageId.split('illust_id=')[1]  # 取出图片ID作为图片名字
-                imgPageURL = self.rootURL + imageId  # 包含原始图片的页面
-                self.saveImage(imgPageURL)
+                imageID = image_item.a['href']
+                self.ID = imageID.split('illust_id=')[1]  # 取出图片ID作为图片名字
+                img_page_url = self.root_url + imageID  # 包含原始图片的页面
+                self.save_image(img_page_url)
 
-    def saveImage(self, imgPageURL):
-        content = self.getUrl(imgPageURL)
-        originalImageItem = content.find(class_='original-image')
-        if originalImageItem is None:
+    def save_image(self, img_page_url):
+        content = self.get_url(img_page_url)
+        original_image_item = content.find(class_='original-image')
+        if original_image_item is None:
             return
         else:
-            print('Downloading', imgPageURL)
-            originalImageUrl = originalImageItem['data-src']
-            filename = originalImageItem['alt']
-            extension = os.path.splitext(originalImageUrl)[1]  # 扩展名
+            print('Downloading', img_page_url)
+            original_image_url = original_image_item['data-src']
+            filename = original_image_item['alt']
+            extension = os.path.splitext(original_image_url)[1]  # 扩展名
 
-            req = request.Request(originalImageUrl)
-            referer = originalImageUrl
+            req = request.Request(original_image_url)
+            referer = original_image_url
             req.add_header('Referer', referer)  # 没有这个header会出现403 error
-            self.removeIllegalChars(filename)
-            path_name = os.path.join(store_path, self.dirName, filename + '_id' + self.ID + extension)
-            try:
-                self.saveImg(req, path_name)  # 捕获图片异常，流程不中断
-            except OSError:
-                pass
+            self.remove_illegal_chars(filename)
+            filename = filename + '_id' + self.ID
+            path_name = os.path.join(STORE_PATH, self.dir_name, filename + extension)
+            txt_name = os.path.join(STORE_PATH, 'ImageName.txt')
+            if self.save_text(txt_name, filename):
+                try:
+                    self.save_img(req, path_name)  # 捕获图片异常，流程不中断
+                except OSError:
+                    pass
+                except urllib.error.HTTPError as e:
+                    pass
+                # except urllib.error.URLError:
+                #     pass
+                except http.client.IncompleteRead:
+                    pass
 
 
-    def removeIllegalChars(self, filename):  # 处理文件名中不合法字符  \ / ? : * " > < |
-            pattern = r'[\\/\?:\*"><\|]*'
-            return re.sub(pattern, "#", filename)
+    @staticmethod
+    def remove_illegal_chars(filename):  # 处理文件名中不合法字符  \ / ? : * " > < |
+        pattern = r'[\\/\?:\*"><\|]*'
+        return re.sub(pattern, "#", filename)
 
 
+class Main(SpiderHTML):
+    def __init__(self):
+        SpiderHTML.__init__(self,COOKIE, USER_AGENT)
+        keyword = u'JK'
+        dir_name = keyword
+        bookmark_count = 1000  # 收藏数目
+        keyword = urllib.parse.quote(keyword)
+        url = START_URL + str(u'&word=' + keyword)
+        print(url)
+        content = self.get_url(url)
 
+        count_badge_text = content.find(class_='count-badge')
+        count_badge = int(count_badge_text.getText().split('件')[0])
+        print(count_badge)
+
+        if count_badge < 4000:
+            page = 10
+        elif count_badge < 6000:
+            page = 15
+        elif count_badge < 8000:
+            page = 20
+        elif count_badge < 10000:
+            page = 25
+        elif count_badge < 12000:
+            page = 30
+        elif count_badge < 14000:
+            page = 35
+        elif count_badge < 16000:
+            page = 40
+        elif count_badge < 18000:
+            page = 45
+        else:
+            page = 50
+
+        page = 1
+
+        # 开启20个线程
+        for i in range(1, 21):
+            _get_img_url = PixivImage(keyword, dir_name, bookmark_count, (i-1)*page+1, i*page)
+            _get_img_url.start()
+
+    #    _get_img_url.join()
 
 if __name__ == '__main__':
-    keyword = u'百合'
-    dirName = keyword
-    bookmarkCount = 1000   # 赞同数量
-    startPage = 1
-    spider = pixivImage(cookie, userAgent, keyword, dirName, bookmarkCount, startPage)
-    spider.start()
-
-
-
+    Main()
+    print('all over')
--- spider.py
+++ spider.py
@@ -1,39 +1,48 @@
 # python3
 # -*- coding: utf-8 -*-
 __author__ = 'allsunny'
-import os, codecs
+import os,codecs,urllib
 from urllib import request
 from bs4 import BeautifulSoup
+from urllib import error
 
 class SpiderHTML(object):
-    def __init__(self, cookie, userAgent):
+    def __init__(self, cookie, user_agent):
         self.cookie = cookie
-        self.userAgent = userAgent
+        self.user_agent = user_agent
 
     # 打开页面
-    def getUrl(self, url, coding='utf-8'):
+    def get_url(self, url, coding='utf-8'):
+
         req = request.Request(url)
-        req.add_header('User-Agent', self.userAgent)
+        req.add_header('User-Agent', self.user_agent)
         req.add_header('Cookie', self.cookie)
         with request.urlopen(req) as response:
-            return BeautifulSoup(response.read().decode(coding), "html.parser")
+            return BeautifulSoup(response.read().decode(coding), "lxml")
+
 
     # 保存文本内容到本地
-    def saveText(self, filename, content, mode='w'):
-        self._checkPath(filename)
+    def save_text(self, filename, content, mode='a+'):
+        self._check_path(filename)
+    #    result = []
         with codecs.open(filename, encoding='utf-8', mode=mode) as f:
-            f.write(content)
-
+            f.seek(0)
+            result = f.read()
+            if content in result:          # 根据图片名查询图片是否已经下载
+                print(content + ' is exist.')
+            else:
+                f.write(content + '\n')
+                return True
 
     # 保存图片
-    def saveImg(self, imgUrl, imgName):
-        data = request.urlopen(imgUrl).read()
-        self._checkPath(imgName)
-        with open(imgName, 'wb') as f:
+    def save_img(self, image_url, image_name):
+        data = request.urlopen(image_url).read()
+        self._check_path(image_name)
+        with open(image_name, 'wb') as f:
             f.write(data)
 
     # 创建目录
-    def _checkPath(self, path):
+    def _check_path(self, path):
         dirname = os.path.dirname(path.strip())
         if not os.path.exists(dirname):
             os.makedirs(dirname)
